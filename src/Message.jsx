cacb32c456ebfc978f3ddee62ed6f22d364c6cf6
Phil <philipchanhk626@gmail.com>
2019-07-02 01:03:17 +0000

    code cleaning

--- src/Message.jsx
+++ src/Message.jsx
@@ -5,27 +5,18 @@ class Message extends Component {
     render() {
         if (this.props.message.type === "incomingMessage") {
             return (
-                
-
                     <div className="message">
-
                         <span className="message-username">{this.props.message.username}</span>
                         <span className="message-content">{this.props.message.content}</span>
                     </div>
-
-                
             )
         } else if (this.props.message.type === "incomingNotification") {
-            return (
-                
+            return (    
                 <div className="message system">
                     {this.props.message.content}
-                </div>
-                
+                </div>    
             )
         }
-
-
     }
 }
 
