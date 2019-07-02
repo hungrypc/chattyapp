49a0a3c270050c38cee6b6069e30ce7c5516d9b4
Phil <philipchanhk626@gmail.com>
2019-07-02 00:49:05 +0000

    jsx files in place

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
+import ChatBar from "./CharBar.jsx";
+import Message from "./Message.jsx";
+import MessageList from "./MessageList.jsx";
+
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <MessageList></MessageList>
+        <ChatBar></ChatBar>
+      </div>
     );
   }
 }
---
+++ src/Message.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+// import MessageList from './MessageList.jsx';
+
+class Message extends Component {
+    render() {
+        if (this.props.message.type === "incomingMessage") {
+            return (
+                
+
+                    <div className="message">
+
+                        <span className="message-username">{this.props.message.username}</span>
+                        <span className="message-content">{this.props.message.content}</span>
+                    </div>
+
+                
+            )
+        } else if (this.props.message.type === "incomingNotification") {
+            return (
+                
+                <div className="message system">
+                    {this.props.message.content}
+                </div>
+                
+            )
+        }
+
+
+    }
+}
+
+export default Message;
